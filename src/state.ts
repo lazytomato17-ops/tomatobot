8c6afea874aeadc0d32c27705585597f91aecd55
lazytomato17-ops <lazytomato17@gmail.com>
2026-04-19 00:00:23 +0900

    state.ts の更新

--- src/state.ts
+++ src/state.ts
@@ -24,7 +24,7 @@ export function createEmptyState(): GameState {
             roles: ['seer'],
             discussionTime: 30,
             autoFinishVoting: true,
-            gayaMode: false,
+            gayaMode: true,
             willMode: false,
             firstNightPeace: false,
             voteTransparency: 'anonymous',
