455ad5d338702057200dd4780cefb7c1612240d2
lazytomato17-ops <lazytomato17@gmail.com>
2026-04-26 09:24:52 +0900

    index.ts を更新

--- src/index.ts
+++ src/index.ts
@@ -37,6 +37,7 @@ const client = new Client({
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
         GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildVoiceStates,
     ],
 });
 
